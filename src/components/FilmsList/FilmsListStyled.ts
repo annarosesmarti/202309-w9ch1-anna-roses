45e1f0090b0231fc80cfce7713b0f1a6ffd4a2c7
Anna Roses <anna.roses.marti@gmail.com>
2023-11-07 23:03:45 +0100

    Add film-container styles

--- src/components/FilmsList/FilmsListStyled.ts
+++ src/components/FilmsList/FilmsListStyled.ts
@@ -4,7 +4,21 @@ const FilmsListStyled = styled.ul`
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 100%;
   gap: 30px;
+
+  .film-container {
+    width: 100%;
+    height: auto;
+    object-fit: contain;
+    aspect-ratio: auto;
+  }
+
+  @media (min-width: 440px) {
+    .film-container {
+      width: 400px;
+    }
+  }
 `;
 
 export default FilmsListStyled;
