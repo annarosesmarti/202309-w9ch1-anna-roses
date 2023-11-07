5d05c27ceef8753028fe4603d777fc407e6280b9
Anna Roses <anna.roses.marti@gmail.com>
2023-11-07 20:00:48 +0100

    Add FilmsListStyled

---
+++ src/components/FilmsList/FilmsListStyled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const FilmsListStyled = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
+`;
+
+export default FilmsListStyled;
