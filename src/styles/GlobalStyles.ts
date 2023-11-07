006850f09fcd991217ac5e2a60e0da8dbe50c426
Anna Roses <anna.roses.marti@gmail.com>
2023-11-07 19:36:31 +0100

    Add GlobalStyles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+*,
+::before,
+::after {
+  box-sizing: border-box;
+}
+
+body,
+h1,
+h2 {
+  margin: 0;
+}
+
+html {
+}
+
+body {
+  font-family: ${({ theme }) => theme.typography.mainFontFamily};
+  font-size: ${({ theme }) => theme.typography.textSize};
+  color: ${({ theme }) => theme.color.mainFontColor};
+  background-color: ${({ theme }) => theme.color.mainBackground};
+}
+
+ul {
+  list-style: none;
+  margin-top: 0;
+  padding: 0;
+}
+`;
+
+export default GlobalStyle;
