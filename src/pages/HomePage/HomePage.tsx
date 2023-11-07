e72240299f057240b82b856a87d636477de9ba07
Anna Roses <anna.roses.marti@gmail.com>
2023-11-07 16:51:10 +0100

    Add dispatcher to HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,27 @@
-const HomePage = (): React.ReactElement => (
-  <header>
-    <h1 className="title">The Hunger Games</h1>
-  </header>
-);
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import useFilmsApi from "../../hooks/useFilmsApi";
+import { loadFilmsActionCreator } from "../../store/features/filmsSlice";
+
+const HomePage = (): React.ReactElement => {
+  const { getFilms } = useFilmsApi();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    (async () => {
+      const currentFilms = await getFilms();
+      dispatch(loadFilmsActionCreator(currentFilms));
+    })();
+  }, [dispatch, getFilms]);
+
+  return (
+    <main>
+      <header className="title-container">
+        <h1 className="title">Harry Potter</h1>
+        <span className="title__text">The movies</span>
+      </header>
+    </main>
+  );
+};
 
 export default HomePage;
