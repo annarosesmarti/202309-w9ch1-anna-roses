7ae2c3e772a178010702fc9d8247a12ca1d0203a
Anna Roses <anna.roses.marti@gmail.com>
2023-11-07 19:30:29 +0100

    Add mainTheme and Styled types

---
+++ src/styles/mainTheme.ts
@@ -0,0 +1,18 @@
+import { DefaultTheme } from "styled-components";
+
+const mainTheme: DefaultTheme = {
+  color: {
+    mainBackground: "#fec844",
+    containerBackground: "#000",
+    mainFontColor: "#000",
+    titleFontColor: "#fec844",
+  },
+  typography: {
+    mainFontFamily: "'Montserrat', 'sans-serif'",
+    titleSize: "2.5rem",
+    subtitleSize: "1.7rem",
+    textSize: "1.25rem",
+  },
+};
+
+export default mainTheme;
