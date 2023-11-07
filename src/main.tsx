82c2fbb2c1c9f90b412a8469c5592b19e8b044fa
Anna Roses <anna.roses.marti@gmail.com>
2023-11-07 12:34:38 +0100

    Fix app and styles links

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/styles.css";
-import App from "./components/App/App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import "./styles/styles.css";
+import App from "./components/App/App";
 import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
