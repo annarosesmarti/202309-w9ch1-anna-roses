0e3dcc4518f096a73537186d00d144d904d8d0ce
Anna Roses <anna.roses.marti@gmail.com>
2023-11-08 17:54:50 +0100

    Add mockData with two films for testing

---
+++ src/mocks/mockData.ts
@@ -0,0 +1,20 @@
+import { FilmsStructure } from "../store/types";
+
+const mockFilms: FilmsStructure[] = [
+  {
+    id: 1,
+    title: "Harry Potter and the Sorcerer's Stone",
+    year: "2001",
+    image:
+      "https://raw.githubusercontent.com/annarosesmarti/202309-w9ch1-api-anna-roses/main/images/film-1.webp",
+  },
+  {
+    id: 2,
+    title: "Harry Potter and the Chamber of Secrets",
+    year: "2002",
+    image:
+      "https://raw.githubusercontent.com/annarosesmarti/202309-w9ch1-api-anna-roses/main/images/film-2.webp",
+  },
+];
+
+export default mockFilms;
